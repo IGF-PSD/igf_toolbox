8cfc785dcca0318079322b2d751eefafbcd03fd5
elissamim <elisamim@gmail.com>
2025-02-20 15:46:25 +0000

    (add) industrial economics imports

--- igf_toolbox/stats_des/__init__.py
+++ igf_toolbox/stats_des/__init__.py
@@ -5,3 +5,4 @@ from .control_secret_stat import (PrimarySecretStatController,
 from .weighted import (_assign_quantile_array, _weighted_quantile_array,
                        assign_quantile, create_pond_data, weighted_quantile,
                        weighted_std)
+from .industrial_economics import *
