183baeb10a57831d3d61ac167a697827f3864933
elissamim <elisamim@gmail.com>
2025-02-20 16:27:53 +0000

    (refacto) remove industrial economics indicators

--- igf_toolbox/stats_des/__init__.py
+++ igf_toolbox/stats_des/__init__.py
@@ -5,4 +5,3 @@ from .control_secret_stat import (PrimarySecretStatController,
 from .weighted import (_assign_quantile_array, _weighted_quantile_array,
                        assign_quantile, create_pond_data, weighted_quantile,
                        weighted_std)
-from .industrial_economics import *
--- igf_toolbox/stats_des/industrial_economics/__init__.py
+++
@@ -1,3 +0,0 @@
-from .market_concentration_indicators import (gini_index, lorenz_curve, 
-                                              hhi, concentration_ratio, 
-                                              shannon_entropy, theil_index)
