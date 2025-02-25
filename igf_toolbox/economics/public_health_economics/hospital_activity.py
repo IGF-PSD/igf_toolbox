f2c65154e580f991db1e859e2826db22eb8a8b90
elissamim <elisamim@gmail.com>
2025-02-25 15:04:21 +0000

    (fix) typo in count number days

--- igf_toolbox/economics/public_health_economics/hospital_activity.py
+++ igf_toolbox/economics/public_health_economics/hospital_activity.py
@@ -1,13 +1,20 @@
 import holidays
 import pandas as pd
 
-def count_number_wroking_days(year:int)->int:
+def count_number_working_days(year:int)->int:
+    """
     """
 
-    """
+    # Define a list of dates of French holidays
+    french_holidays_dates=holidays.France(years=year)
 
-    french_holidays=holidays.France(years=year)
+    # Count the number of working days and non working days (i.e weekends and holidays)
     dates=pd.date_range(start=f"{year}-01-01", end=f"{year}-12-31", freq="D")
+    number_working_days=sum(1 for date in dates 
+                            if (date.weekday() < 5) and (date not in french_holidays_dates))
+    number_non_working_days=len(dates)-number_working_days
+
+    return number_working_days, number_non_working_days
 
 def effet_cjo(year:int, activity:str="volume")->float:
     """
@@ -18,9 +25,9 @@ def effet_cjo(year:int, activity:str="volume")->float:
     Returns:
 
     Raises:
-    
     """
 
+    # Define the weight of the non working days
     if activity=="volume":
         coeff=.49
     elif activity=="séjours":
@@ -29,11 +36,13 @@ def effet_cjo(year:int, activity:str="volume")->float:
         raise ValueError("""
                         `activity` parameter should be in [`volume`, `séjours`] for holiday adjustments.
                         For `volume` a 49% correction is applied, for `séjours` a 34% correction is applied.
-                        
+                        `séjours` is applied for both stays and `équivalents-journées`.
                         """)
 
-    jours_activite_n=
-    jours_activite_n_1=
+    # Compute the number of `jours d'activite` and return their evolution
+    # as CJO adjustment
+    jours_activite_n=count_number_working_days(year)[0]+coeff*count_number_working_days(year)[1]
+    jours_activite_n_1=count_number_working_days(year-1)[0]+coeff*count_number_working_days(year-1)[1]
 
     return jours_activite_n/jours_activite_n_1-1
     
