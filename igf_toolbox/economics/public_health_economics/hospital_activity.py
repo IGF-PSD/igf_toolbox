d4b03dee41ce92efbb175f5913faee3373dea6a2
elissamim <elisamim@gmail.com>
2025-02-26 13:17:08 +0000

    (wip) effet structure breakdown

--- igf_toolbox/economics/public_health_economics/hospital_activity.py
+++ igf_toolbox/economics/public_health_economics/hospital_activity.py
@@ -5,17 +5,14 @@ import pandas as pd
 
 
 class HospitalActivity:
-    def __init__(self, data, ghm, ghs, prefix_stays, price=None, prefix_dms=None):
+    def __init__(self, data, ghm, prefix_stays):
         """ 
         
         """
 
         self.data = data
         self.ghm = ghm
-        self.ghs = ghs
-        self.price = price
         self.prefix_stays = prefix_stays
-        self.prefix_dms = prefix_dms
 
         self.years = sorted(
             [
@@ -25,7 +22,7 @@ class HospitalActivity:
             ]
         )
 
-    def count_number_working_days(self, year: int) -> tuple[int]:
+    def _count_number_working_days(self, year: int) -> tuple[int]:
         """
         Returns the number of working and non working days in a given year.
 
@@ -51,7 +48,7 @@ class HospitalActivity:
 
         return number_working_days, number_non_working_days
 
-    def effet_cjo(self, year: int, activity: str = "volume") -> float:
+    def _effet_cjo(self, year: int, activity: str = "volume") -> float:
         """
         Returns the holiday adjustments to apply to yearly evolutions
         between year and year - 1.
@@ -84,25 +81,83 @@ class HospitalActivity:
         # Compute the number of `jours d'activite` and return their evolution
         # as CJO adjustment
         jours_activite_n = (
-            self.count_number_working_days(year)[0]
-            + coeff * self.count_number_working_days(year)[1]
+            self._count_number_working_days(year)[0]
+            + coeff * self._count_number_working_days(year)[1]
         )
         jours_activite_n_1 = (
-            self.count_number_working_days(year - 1)[0]
-            + coeff * self.count_number_working_days(year - 1)[1]
+            self._count_number_working_days(year - 1)[0]
+            + coeff * self._count_number_working_days(year - 1)[1]
         )
 
         return jours_activite_n / jours_activite_n_1 - 1
 
-    def effet_volume(self) -> pd.DataFrame:
-        """ """
+    def _prix_apparent(self, ghs, nber_stays_eps, amount_eps, rate_eps)->pd.DataFrame:
+        """ 
+        Returns the average price of a GHMxGHS.
 
-        data = self.data.groupby(
-            by=[self.ghm, self.ghs], as_index=False
-        ).sum()  # TO CHANGE
+        Args:
+        """
+
+        data_price=self.data.groupby(by=[self.ghm, ghs],
+                                    as_index=False)[[nber_stays_eps,
+                                                    amount_eps,
+                                                    rate_eps]+
+                                                    [col for col in self.data.columns
+                                                    if col.startswith(self.prefix_stays)]].sum()
+        
+        data_price["prix_apparent"]=data_price[[nber_stays_eps, 
+                                              amount_eps, 
+                                              rate_eps]].apply(lambda x: 0 if x[0]==0 or x[2]==0 else (1/x[0])*(x[1]/x[2]), 
+                                                               axis=1)
+
+        return data_price
+
+    def _prix_apparent_breakdown(by, nber_stays_eps, amount_eps, rate_eps)->pd.DataFrame:
+        """
+        Returns a dataframe with an average price for a given breakdown of effet volume.
+
+        Args:
+
+        Returns:
+        
+        """
+
+        data_price=self.data.copy(deep=True)
+        
+        data_price=data_price.groupby(by=by,
+                                     as_index=False).agg(
+            sum_stays_eps=(nber_stays_eps, "sum"),
+            sum_amount_eps=(amount_eps,lambda x: ((x/data_price.loc[x.index, rate_eps]).where(data_price.loc[x.index,rate_eps]!=0, 0)).sum())
+                                     )
+        
+        data_price["prix_apparent"]=data_price[[sum_stays_eps,
+                                               sum_amount_eps]].apply(lambda x: 0 if x[0]==0 else (1/x[0])*x[1])
+        return data_price
+
+    def _effet_structure_components():
+        pass
+
+    def effet_volume(self, ghs, nber_stays_eps, amount_eps, rate_eps) -> pd.DataFrame:
+        """ 
+        Returns effet volume, its calendar adjustment and all its components.
+
+        Args:
+
+        Returns:
+
+        Raises:
+        """
+
+        data = self._prix_apparent(ghs, nber_stays_eps, amount_eps, rate_eps)
+
+        data = data.groupby(
+            by=[self.ghm, ghs], as_index=False
+        ).agg({"prix_apparent":"mean", 
+               **dict.fromkeys([col for col in data.columns
+                                if col.startswith(self.prefix_stays)], "sum")})
 
         dict_volume_economique = {
-            year: (data[self.price] * data[f"{self.prefix_stays}{year}"]).sum()
+            year: (data["prix_apparent"] * data[f"{self.prefix_stays}{year}"]).sum()
             for year in self.years
         }
 
@@ -112,7 +167,7 @@ class HospitalActivity:
         }
 
         dict_effet_volume_cjo = {
-            year: dict_effet_volume[year] - self.effet_cjo(year)
+            year: dict_effet_volume[year] - self._effet_cjo(year)
             for year in self.years[1:]
         }
 
@@ -123,12 +178,29 @@ class HospitalActivity:
             for year in self.years[1:]
         }
 
+
+
+        # We compute the effet racine
+        data_racine = self.data.copy(deep=True)
+        data_racine["racine"] = data_racine[self.ghm].str[:5]
+        data_racine = self._prix_apparent_breakdown("racine",
+                                                   nber_stays_eps,
+                                                   amount_eps,
+                                                   rate_eps)
+
+        # We compute the effet bascule vers l'ambulatoire
+
+        # We compute the effet sévérité
+
         data_activity = pd.DataFrame(
             {
                 "Volume économique": dict_volume_economique,
                 "Effet volume": dict_effet_volume,
                 "Effet volume CJO": dict_effet_volume_cjo,
                 "Effet nombre de séjours": dict_effet_nombre_de_sejours,
+                "Effet racine":dict_effet_racine,
+                "Effet bascule vers l'ambulatoire":dict_effet_bascule_ambulatoire,
+                "Effet sévérité":dict_effet_severite
             }
         )
 
@@ -136,17 +208,6 @@ class HospitalActivity:
         data_activity["Effet structure"] = (
             data_activity["Effet volume"] - data_activity["Effet nombre de séjours"]
         )
-
-        # We compute the effet racine
-        data_racine = data_activity.copy(deep=True)
-        data_racine["racine"]=data_racine[self.ghm].apply(lambda x: x[:5])
-        data_racine=data_racine.drop(columns=self.ghs)
-        data_racine=data_racine.groupby("racine",
-                                       as_index=False).agg({})
-
-        # We compute the effet bascule vers l'ambulatoire
-
-        # We compute the effet sévérité
 
         # We compute the effet résiduel
         data_activity["Effet résiduel"] = (
@@ -157,3 +218,8 @@ class HospitalActivity:
         )
 
         return data_activity
+
+    # def evolution_equivalents_journees(self, prefix_dms):
+    #     """ """
+
+    #     pass
